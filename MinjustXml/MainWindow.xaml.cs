2043b550e64f66344cf1d2b5ff0a97ee8c4facc1
David <david.chalyan25@gmail.com>
2022-09-17 15:16:48 +0300

    добавил дату платежного поручения, а также поместил открытие xml файла в блок try/catch

--- MinjustXml/MainWindow.xaml.cs
+++ MinjustXml/MainWindow.xaml.cs
@@ -3,19 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Mail;
-using System.Net.Mime;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Xml;
 using System.Xml.Serialization;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MinjustXml
 {
@@ -25,6 +16,11 @@ namespace MinjustXml
     public partial class MainWindow : Window
     {
         private System.DateTime date_pp;
+        public string GetPP
+        {
+            get { return date_pp.ToString("dd.MM.yyyy"); }
+
+        }
         List<RegPP> regs = new List<RegPP>();
         public MainWindow()
         {
@@ -33,14 +29,14 @@ namespace MinjustXml
         OpenFileDialog openFileDialog = new OpenFileDialog()
         {
             Multiselect = true,
-            Title="Выберите файлы "
+            Title = "Выберите файлы "
         };
 
         public void Open_Click(object sender, RoutedEventArgs e)
         {
             openFileDialog.Filter = "XML files (*.XML)|*.XML";
             openFileDialog.ShowDialog();
-            
+
             if (openFileDialog.FileName == String.Empty)
             {
                 return;
@@ -52,43 +48,52 @@ namespace MinjustXml
             }
             peoplesGrid.ItemsSource = regs;
         }
-        private void OpenXml( string fileName)
+        private void OpenXml(string fileName)
         {
-            string text = "";
-            using (StreamReader reader = new StreamReader(fileName))
+            try
             {
-                text = reader.ReadToEnd();
-            }
-            XmlDocument doc = new XmlDocument();
-            doc.Load(fileName);
-            XmlNodeList elemList = doc.GetElementsByTagName("RegPP");
-            var zxc = doc.GetElementsByTagName("DATE_PP");
-            date_pp = DateTime.Parse(zxc[0].InnerXml);
-            var rootAttribute = new XmlRootAttribute();
-            rootAttribute.ElementName = "RegPP";
-            XmlSerializer serializer = new XmlSerializer(typeof(RegPP), rootAttribute);
-            for (int i = 1; i < elemList.Count; i++)
-            {
-                using (StringReader reader = new StringReader(elemList[i].InnerXml))
+                string text = "";
+                using (StreamReader reader = new StreamReader(fileName))
                 {
-                    var k = elemList[i].InnerXml.Insert(0, "<RegPP>");
-                    k = k.Insert(k.Length, "</RegPP>");
-                    var s = (RegPP)serializer.Deserialize(new StringReader(k));
-                    regs.Add(s);
+                    text = reader.ReadToEnd();
+                }
+                XmlDocument doc = new XmlDocument();
+                doc.Load(fileName);
+                XmlNodeList elemList = doc.GetElementsByTagName("RegPP");
+                var zxc = doc.GetElementsByTagName("DATE_PP");
+                date_pp = DateTime.Parse(zxc[0].InnerXml);
 
+                var rootAttribute = new XmlRootAttribute();
+                rootAttribute.ElementName = "RegPP";
+                XmlSerializer serializer = new XmlSerializer(typeof(RegPP), rootAttribute);
+                for (int i = 1; i < elemList.Count; i++)
+                {
+                    using (StringReader reader = new StringReader(elemList[i].InnerXml))
+                    {
+                        var k = elemList[i].InnerXml.Insert(0, "<RegPP>");
+                        k = k.Insert(k.Length, "</RegPP>");
+                        var s = (RegPP)serializer.Deserialize(new StringReader(k));
+                        regs.Add(s);
+
+                    }
+                }
+                Regex regex = new Regex(".*ОПЛАТА ЗА:(.*)");
+                foreach (var reg in regs)
+                {
+                    reg.PURPOSE = regex.Replace(reg.PURPOSE, "$1");
+                }
+                Regex regex1 = new Regex("(.*)00");
+                foreach (var reg in regs)
+                {
+                    reg.SUM_REESTR_PP = regex1.Replace(reg.SUM_REESTR_PP, "$1.00");
                 }
             }
-            Regex regex = new Regex(".*ОПЛАТА ЗА:(.*)");
-            foreach (var reg in regs)
+            catch (Exception ex)
             {
-                reg.PURPOSE = regex.Replace(reg.PURPOSE, "$1");
-            }
-            Regex regex1 = new Regex("(.*)00");
-            foreach (var reg in regs)
-            {
-                reg.SUM_REESTR_PP= regex1.Replace(reg.SUM_REESTR_PP, "$1.00");
+                MessageBox.Show("Ошибка чтения XML файла.");
             }
             countregs.Text = "Количество человек: " + regs.Count().ToString();
+            datepp.Text = "Дата платежного поручения: " + GetPP;
         }
     }
 }
